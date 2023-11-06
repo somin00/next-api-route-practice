703b17e1eec3105b95d77c9fb998b0a3b7f7d9ec
somin <smhs12@naver.com>
2023-11-06 17:45:57 +0900

    feat: 동적 API 라우트 사용

--- pages/feedback.js
+++ pages/feedback.js
@@ -1,15 +1,26 @@
 import { getFileData, getFilePath } from "./api/feedback"; //클라이언트 사이드 번들에 포함되지 않음
-
+import { useState } from "react";
 export default function Feedback(props) {
   const { feedback } = props;
+
+  const [detail, setDetail] = useState({});
+
+  function showFeedbackDetail(id) {
+    fetch(`/api/${id}`)
+      .then((res) => res.json())
+      .then((data) => setDetail(data.feedback));
+  }
   return (
-    <ul>
-      {feedback.map((data) => (
-        <li key={data.id}>
-          {data.email} : {data.message}
-        </li>
-      ))}
-    </ul>
+    <>
+      {detail && <p>{detail.email}</p>}
+      <ul>
+        {feedback.map((data) => (
+          <li key={data.id}>
+            {data.message} <button onClick={showFeedbackDetail.bind(null, data.id)}>상세보기</button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
